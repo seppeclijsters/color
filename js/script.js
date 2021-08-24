86b306656957de6226c7f137cd5d127baa3f8f21
seppe <seppe.clijsters@hotmail.com>
2021-08-24 21:55:20 +0200

    drag cells

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 // Color
 let currentColor = "red";
+let mouseDown = false;
+document.addEventListener("mousedown", () => mouseDown = true);
+document.addEventListener("mouseup", () => mouseDown = false);
 
 // ----- Draw grid -----
 const container = document.getElementById("container");
 
+var h = window.innerHeight;
+console.log('heigh is: ' + h)
+let cols = parseInt(h / 20);
+console.log(cols);
+
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('--grid-cols', rows);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     // cell.innerText = (c + 1);
@@ -14,17 +22,23 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(100, 100);
+makeRows(100, cols);
 
 
 // ----- Draw cells -----
 const drawCell = (e) => {
-    console.log(e);
-    console.log(e.currentTarget);
+    // console.log(e);
+    // console.log(e.currentTarget);
     e.currentTarget.classList.toggle('is-active');
-    if (!e.currentTarget.classList.contains('is-active')) {
-        e.currentTarget.style.backgroundColor = "unset";
+    if (!e.currentTarget.classList.contains('is-active') ) {
+        e.currentTarget.style.backgroundColor = "white";
     } else {
+        e.currentTarget.style.backgroundColor = currentColor;
+    }
+}
+const selectCell = (e) => {
+    if (!e.currentTarget.classList.contains('is-active') && mouseDown) {
+        e.currentTarget.classList.add('is-active');
         e.currentTarget.style.backgroundColor = currentColor;
     }
 }
@@ -32,7 +46,12 @@ const drawCell = (e) => {
 let cells = document.querySelectorAll('.grid-item');
 cells.forEach(cell => {
     cell.addEventListener('click', drawCell);
+    cell.addEventListener('mousemove', selectCell);
 });
+
+
+
+// document.addEventListener('mousedown', selectCell);
 
 // ----- Right click -> pick color -----
 const pickColor = (e) => {
@@ -42,19 +61,19 @@ const pickColor = (e) => {
     // Mouse coordinates
     let x = e.clientX;
     let y = e.clientY;
-    console.log(y);
-    console.log(x);
+    // console.log(y);
+    // console.log(x);
     let colorPicker = document.querySelector('.color-picker');
-    console.log(colorPicker);
+    // console.log(colorPicker);
     colorPicker.style.top = (y - "16") + "px";
-    colorPicker.style.left = (x - "67.5") + "px";
+    colorPicker.style.left = (x - "80") + "px";
 
     // Animation hide/show color picker
     if (colorPickers.classList.contains('hide')) {
-        console.log('verstopt');
+        // console.log('verstopt');
         colorPickers.classList.add('show');
     } else {
-        consol.log('niet verstopt');
+        // consol.log('niet verstopt');
     }
 }
 
@@ -89,3 +108,6 @@ const chooseColor = (e) => {
 colors.forEach(color => {
     color.addEventListener('click', chooseColor)
 })
+
+
+
